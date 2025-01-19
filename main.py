262932fcd3034609148906dad8fcf13cc22034ae
PoulYak <pavlik.yakovlev.2004@gmail.com>
2025-01-19 10:13:12 +0300

    changed smth

--- main.py
+++ main.py
@@ -1,38 +1,53 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 import uvicorn
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
 
-
-
-# Инициализация приложения FastAPI
 app = FastAPI(
     title="LLaMA 3-Instruct API",
     description="API для инференса LLaMA 3-Instruct",
     version="1.0.0",
 )
 
-# Подключение статики
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Или укажите конкретные адреса, например, ["http://localhost:3000"]
+    allow_credentials=True,
+    allow_methods=["*"],  # Разрешить все методы: GET, POST, OPTIONS, PUT, DELETE и т.д.
+    allow_headers=["*"],  # Разрешить все заголовки
+)
 
 # Инициализация модели и токенайзера
 MODEL_NAME = "meta-llama/Meta-Llama-3.1-8B-Instruct"  # Укажите имя модели из Hugging Face
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-quantization_config = BitsAndBytesConfig(load_in_4bit=True,
-                                         bnb_4bit_compute_dtype=torch.float16)
-
 try:
     tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
     tokenizer.pad_token = tokenizer.eos_token
+    if device == "cuda":
+        print('cuda')
+        from transformers import BitsAndBytesConfig
 
-    model = AutoModelForCausalLM.from_pretrained(MODEL_NAME,
-                                                 quantization_config=quantization_config,
-                                                 device_map="auto",
-                                                 torch_dtype=torch.float16)
+        quantization_config = BitsAndBytesConfig(load_in_4bit=True,
+                                                 bnb_4bit_compute_dtype=torch.float16)
+
+        model = AutoModelForCausalLM.from_pretrained(MODEL_NAME,
+                                                     quantization_config=quantization_config,
+                                                     device_map="auto",
+                                                     torch_dtype=torch.float16)
+    else:
+        print('cpu')
+        model = None
+        # model = AutoModelForCausalLM.from_pretrained(MODEL_NAME,
+        #                                              device_map='cpu',
+        #                                              low_cpu_mem_usage=True)
+        #
+        # model = torch.quantization.quantize_dynamic(
+        #     model, {torch.nn.Linear}, dtype=torch.qint8
+        # )
+
 except Exception as e:
     raise RuntimeError(f"Ошибка загрузки модели: {e}")
 
@@ -50,6 +65,7 @@ class InferenceResponse(BaseModel):
     generated_text: str
     answer: str
 
+
 def get_last_answer(text):
     return text.split('<|end_header_id|>\n\n')[-1].replace('<|eot_id|>', '')
 
@@ -60,45 +76,49 @@ def make_prompt_chat(chat):
         role = message['role']
         content = message['content']
         prompt.append(f"<|start_header_id|>{role}<|end_header_id|>\n\n")
-        if role=='system':
+        if role == 'system':
             prompt.append('Cutting Knowledge Date: December 2024\nToday Date: 28 Dec 2024\n\n')
         if content:
             prompt.append(content)
             prompt.append(f"<|eot_id|>")
     return ''.join(prompt)
 
+
 def get_prompt(text):
     prompt = """<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\nCutting Knowledge Date: December 2024\nToday Date: 28 Dec 2024\n\nYou are assistant.<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n{message}<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n"""
     return prompt.format(message=text)
 
 
-# Корневой маршрут
-@app.get("/")
-def redirect_to_ui():
-    return RedirectResponse(url="/static/index.html")
+d = {'counter': 0}
 
 
 @app.post("/generate", response_model=InferenceResponse)
 def generate_text(request: InferenceRequest):
     try:
-        # Токенизация ввода
-        inputs = tokenizer(get_prompt(request.prompt), return_tensors="pt", padding=True, truncation=True).to(device)
 
-        # Установка attention_mask
-        attention_mask = inputs["attention_mask"]
-        # Генерация текста
-        outputs = model.generate(
-            inputs["input_ids"],
-            attention_mask=inputs["attention_mask"],
-            max_length=request.max_length,
-            temperature=request.temperature,
-            top_k=request.top_k,
-            do_sample=True,
-            pad_token_id=tokenizer.pad_token_id
-        )
+        if device == "cuda":
+            # Токенизация ввода
+            inputs = tokenizer(get_prompt(request.prompt), return_tensors="pt", padding=True, truncation=True).to(
+                device)
 
-        # Декодирование результата
-        generated_text = tokenizer.decode(outputs[0])
+            # Установка attention_mask
+            attention_mask = inputs["attention_mask"]
+            # Генерация текста
+            outputs = model.generate(
+                inputs["input_ids"],
+                attention_mask=inputs["attention_mask"],
+                max_length=request.max_length,
+                temperature=request.temperature,
+                top_k=request.top_k,
+                do_sample=True,
+                pad_token_id=tokenizer.pad_token_id
+            )
+
+            # Декодирование результата
+            generated_text = tokenizer.decode(outputs[0])
+        else:
+            d['counter'] += 1
+            generated_text = get_prompt(request.prompt) + f'Ответ {d["counter"]} от модели cpu' + '<|eot_id|>'
         answer = get_last_answer(generated_text)
         return InferenceResponse(generated_text=generated_text, answer=answer)
 
